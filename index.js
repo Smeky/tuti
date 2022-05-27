1d3ea4c14d6ec8d156be3eb72c5a7fe983c11c37
Smeky <smeky.sobota@gmail.com>
2022-05-28 00:03:10 +0200

    Added ability to run utility modules

---
+++ index.js
@@ -0,0 +1,18 @@
+const { program } = require("commander")
+const { fork } = require("child_process")
+const packagejson = require("./package.json")
+
+program
+    .name(packagejson.name)
+    .description(packagejson.description)
+    .version(packagejson.version)
+
+program
+    .command("notes")
+    .description("[Todo: fill description from utility package.json]")
+    .argument("[args...]")
+    .action((args) => {
+        fork("utilities/notes", args)
+    })
+
+program.parse()
---
+++ utilities/notes/index.js
@@ -0,0 +1,15 @@
+const { program } = require("commander")
+const packagejson = require("./package.json")
+
+program
+    .name(packagejson.name)
+    .description(packagejson.description)
+    .version(packagejson.version)
+
+program.command('standup')
+    .description('Create a new notion page from template with current date as title')
+    .action(() => {
+        console.log("Notes standup run");
+    })
+
+program.parse()
